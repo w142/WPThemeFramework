36f2595f9a9d0ef297f62b1c8f70f3f3d57c54f9
Torsten Brandt <brandt@w142.de>
2010-12-15 11:23:01 +0100

    updated index.php to HTML5
    Signed-off-by: Torsten Brandt <brandt@w142.de>

--- layout/index.php
+++ layout/index.php
@@ -9,16 +9,15 @@
 /*                                                                            */
 /******************************************************************************/
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="de-DE">
+<!DOCTYPE html>
+<html lang="de-DE">
 	<head profile="http://gmpg.org/xfn/11">
 		<title></title>
 		<meta name="keywords" content="" />
 		<meta name="description" content="" />
 		<meta name="author" content="" />
 		<meta name="language" content="de" />
-		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-		<meta http-equiv="content-style-type" content="text/css" />
+		<meta charset="utf-8" />
 		<meta http-equiv="content-script-type" content="text/javascript" />
 		<meta name="robots" content="noindex, nofollow" />
 <!--
@@ -27,7 +26,7 @@
 -->
 		<meta name="viewport" content="width=device-width, initial-scale = 1.0" />
 <!-- 	Links to css file which has to be compiled from the LESS file in the css folder		 -->
-		<style type="text/css" media="screen">@import url("css/style.css");</style>
+		<link rel="stylesheet" href="css/style.css" />
 <!-- 	Links to the favicon  -->
 		<link rel="shortcut icon" href="img/favicon.ico" />
 <!--
@@ -35,15 +34,18 @@
 		@see http://developer.apple.com/library/safari/#documentation/AppleApplications/Reference/SafariWebContent/ConfiguringWebApplications/ConfiguringWebApplications.html
 -->
 		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+		
+<!-- 	Enable HTML5 Elements in Internet Explorer -->
+		<!--[if lt IE 9]> <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script> <![endif]-->
 	</head>
 	<body>
 		
 <!--
 		Links to version 1.4.4 of the jQuery Library hosted on Google. If loading fails a version in js/ is used
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
-		<script>!window.jQuery && document.write(unescape('%3Cscript src="js/jquery-1.4.4.js"%3E%3C/script%3E'))</script>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+		<script "text/javascript">!window.jQuery && document.write(unescape('%3Cscript src="js/jquery-1.4.4.js"%3E%3C/script%3E'))</script>
 -->
 <!-- 	Links to the script file in the js folder. Your JavaScript goes in here -->
-		<script src="js/script.js"></script>
+		<script type="text/javascript" src="js/script.js"></script>
 	</body>
 </html>
