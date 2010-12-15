d0e5eaf8b5129b65ce591f974dd7442821ff3584
Torsten Brandt <brandt@w142.de>
2010-12-15 10:03:32 +0100

    added template content to index.php
    Signed-off-by: Torsten Brandt <brandt@w142.de>

---
+++ layout/index.php
@@ -0,0 +1,49 @@
+<?php
+/******************************************************************************/
+/*                                                                            */
+/*  W142 – Index Page                                                         */
+/*  http://www.w142.de                                                        */
+/*                                                                            */
+/*  author:	Torsten Brandt <brandt@w142.de>                                   */
+/*  web:	http://www.w142.de                                                */
+/*                                                                            */
+/******************************************************************************/
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="de-DE">
+	<head profile="http://gmpg.org/xfn/11">
+		<title></title>
+		<meta name="keywords" content="" />
+		<meta name="description" content="" />
+		<meta name="author" content="" />
+		<meta name="language" content="de" />
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="content-style-type" content="text/css" />
+		<meta http-equiv="content-script-type" content="text/javascript" />
+		<meta name="robots" content="noindex, nofollow" />
+<!--
+		Set devicewith to correct width on apple devices
+		@see http://developer.apple.com/library/safari/#documentation/AppleApplications/Reference/SafariHTMLRef/Articles/MetaTags.html
+-->
+		<meta name="viewport" content="width=device-width, initial-scale = 1.0" />
+<!-- 	Links to css file which has to be compiled from the LESS file in the css folder		 -->
+		<style type="text/css" media="screen">@import url("css/style.css");</style>
+<!-- 	Links to the favicon  -->
+		<link rel="shortcut icon" href="img/favicon.ico" />
+<!--
+		Link to a Icon used by apple devices if Page is put on homescreen
+		@see http://developer.apple.com/library/safari/#documentation/AppleApplications/Reference/SafariWebContent/ConfiguringWebApplications/ConfiguringWebApplications.html
+-->
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+	</head>
+	<body>
+		
+<!--
+		Links to version 1.4.4 of the jQuery Library hosted on Google. If loading fails a version in js/ is used
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+		<script>!window.jQuery && document.write(unescape('%3Cscript src="js/jquery-1.4.4.js"%3E%3C/script%3E'))</script>
+-->
+<!-- 	Links to the script file in the js folder. Your JavaScript goes in here -->
+		<script src="js/script.js"></script>
+	</body>
+</html>
